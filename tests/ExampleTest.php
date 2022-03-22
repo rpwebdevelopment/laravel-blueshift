7bce75ce8400c8a4819142ce21148ca2bf5407a9
Rich Porter <rich.porter@carandclassic.com>
2022-03-22 09:27:30 +0000

    cleaning up tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,19 +6,12 @@ namespace Rpwebdevelopment\LaravelBlueshift\Tests;
 
 use Rpwebdevelopment\LaravelBlueshift\Contracts\BlueshiftCatalog;
 use Rpwebdevelopment\LaravelBlueshift\Exceptions\BlueshiftValidationException;
-use Rpwebdevelopment\LaravelBlueshift\Services\Api\Api;
 
 class ExampleTest extends TestCase
 {
     public function testTrueIsTrue(): void
     {
         $this->assertTrue(true);
-    }
-
-    public function testApiCorrectInstance(): void
-    {
-        $api = app()->make(Api::class);
-        $this->assertInstanceOf(Api::class, $api);
     }
 
     public function testCatalogValidateReturnsValid(): void
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -33,10 +33,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-blueshift_table.php.stub';
-        $migration->up();
-        */
     }
 }
